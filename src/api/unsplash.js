8b3588f37ff370f47bac710bd7b79663b857b540
Ahmed <26268158+ahmed316dev@users.noreply.github.com>
2022-11-20 12:31:00 +0300

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default axios.create({
     baseURL: 'https://api.unsplash.com/',
     headers: {
-        Authorization:'Client-ID AaPCllor4aNOBqLVp8b1NV9c3yiMUC7REOJMdXMyEFQ', 
+        Authorization:process.env.UNSPLASH_AUTH, 
     }
 
 })
